da8b8ba620db384366e71c23efd77c4b8f907722
Martin Iglesias <72715575+MartinIglesias86@users.noreply.github.com>
2022-05-19 13:31:07 -0300

    working on index.js

--- pages/index.js
+++ pages/index.js
@@ -264,7 +264,7 @@ export default function Home() {
           }
         }
       }, 5 * 1000);
-      // set an interval to get the number of token Ids minted every 5 seconds
+      //set an interval to get the number of token Ids minted every 5 seconds
       setInterval(async function () {
         await getTokenIdsMinted();
       }, 5 * 1000);
